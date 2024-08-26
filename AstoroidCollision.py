e41d8399b078f17ae115ef8d3d4ce6b70b9f2c7a
Nilmani Chandrakar <46774689+nilmanichandrakar@users.noreply.github.com>
2024-08-26 23:55:50 +0530

    AstoroidCollision using Stack

---
+++ AstoroidCollision.py
@@ -0,0 +1,14 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stck = []
+        for x in asteroids:
+            if x > 0:
+                stck.append(x)
+            else:
+                while stck and stck[-1] > 0 and stck[-1] < -x:
+                    stck.pop()
+                if stck and stck[-1] == -x:
+                    stck.pop()
+                elif not stck or stck[-1] < 0:
+                    stck.append(x)
+        return stck
